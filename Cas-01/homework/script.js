5b5e21a1289edf6a6131ca9467d50a824967457f
stfmdc <mandicstefan94@gmail.com>
2024-01-17 00:46:05 +0100

    primeri-sa-casa

---
+++ Cas-01/homework/script.js
@@ -0,0 +1,30 @@
+// Exercise - feet to meters
+var feet = 0.3048;
+var meters = 10;
+var length = feet * meters;
+console.log("Duzina 10 stopa iznosi", length, "metara.");
+
+// Exercise - povrsina pravougaonika
+var a = 5;
+var b = 15;
+var c = a * b;
+var string = "Povrsina pravougaonika je";
+console.log(string, c, "metara.");
+
+// Exercize - povrsina kruga
+var r = 10; // poluprecnik
+var precnik = r * r;
+var pi = 3.14; // priblizno jer nismo ucili jos math.pi
+var area = pi * precnik;
+var string = "Povrsina kruga je";
+console.log(string, area, "centimetara.");
+
+// Exercize - obim kruga
+var r = 10; // poluprecnik
+var pi = 3.14 // priblizno jer nismo ucili jos math.pi
+var area = 2 * r * pi;
+var string = "Obim kruga iz gornje linije koda je";
+console.log(string, area, "centimetara.");
+
+// Homework - kupovina 30 telefona sa ukljuneom taksom od 5%
+var 
---
+++ Cas-01/script.js
@@ -0,0 +1,81 @@
+/*
+function addNumbers (number1, number2)
+        {
+            var result = number1 + number2;
+            console.log(result);
+        }
+        addNumbers(2, 2);
+*/
+/*
+var testVar = 100;
+alert(testVar); //pokazuje null
+alert(typeof testVar); //pokazuje object
+
+var testVar2 = "Perica";
+alert(testVar2);
+alert(typeof testVar2);
+*/
+
+var firstName = "Marko";
+var lastName = "Markovic";
+console.log(firstName, "firstName")
+console.log(lastName, "lastName")
+
+var first = 1; //number
+console.log(first, "first")
+
+var age = "24"; //string
+console.log(age, "age")
+
+var numberAge = 24; //number
+console.log(numberAge, "numberAge")
+
+var decimalNumber = 2.5; //number
+console.log(decimalNumber, "decimalNumber")
+
+var knowsHtml = true; //boolean
+var knowJS = false;
+console.log(knowsHtml, "knowsHtml")
+console.log(knowJS, "knowJS")
+
+//arithmetic operators
+var a = 5;
+var b = 2;
+var c;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+console.log(a++); //racuna se ali se ne prikaze do sl. operacije !!!
+console.log(a);
+console.log(--b);
+
+//comparison operators
+var x = 5;
+var y = 2;
+console.log(x == y); //false
+console.log(x != y); //true
+console.log(x > y); //true
+console.log(x < y); //false
+console.log(x >= y); //true
+console.log(x <= y); //false
+
+//assignment operators
+var x = 5;
+x += 2; //7
+console.log(x);
+x -= 2; //5
+console.log(x);
+x *= 2; //10
+console.log(x);
+x /= 2; //5
+console.log(x);
+x %=2; //1
+console.log(x);
+
+var a = 5;
+var b = 10;
+var c = a * b;
+console.log(c);
+
