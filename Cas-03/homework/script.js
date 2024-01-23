b33112f3d06c9392e5d46cf758dabbe4667f1fe6
stfmdc <mandicstefan94@gmail.com>
2024-01-23 22:19:36 +0100

    homeworks-atm-and-days-in-month

--- Cas-03/homework/script.js
+++ Cas-03/homework/script.js
@@ -2,9 +2,9 @@
 
 let stanje = 1000;
 function atm() {
-    let iznos = parseFloat(prompt("Unesite iznos koji želite podići:"));
+    let iznos = parseFloat(prompt(`Unesite iznos koji želite podići:`));
     if (isNaN(iznos) || iznos <= 0) {
-      alert("Molimo unesite validan iznos.");
+      alert(`Molimo unesite validan iznos.`);
       return;
     }
     if (iznos > stanje) {
@@ -18,7 +18,7 @@ function atm() {
 // homework - dani u mesecu
 
 function brojDanaUMesecu() {
-let nazivMeseca = prompt("Unesite naziv meseca:").toLowerCase();
+let nazivMeseca = prompt(`Unesite naziv meseca:`).toLowerCase();
   
 switch (nazivMeseca) {
     case "januar":
@@ -28,29 +28,29 @@ switch (nazivMeseca) {
     case "avgust":
     case "oktobar":
     case "decembar":
-        alert("Ovaj mesec ima 31 dan.");
+        alert(`Ovaj mesec ima 31 dan.`);
         break;
   
     case "april":
     case "jun":
     case "septembar":
     case "novembar":
-        alert("Ovaj mesec ima 30 dana.");
+        alert(`Ovaj mesec ima 30 dana.`);
         break;
   
     case "februar":
-        let godina = parseInt(prompt("Unesite godinu:"));
+        let godina = parseInt(prompt(`Unesite godinu:`));
         let prestupna = (godina % 4 === 0 && godina % 100 !== 0) || (godina % 400 === 0);
   
         if (prestupna) {
-          alert("Februar u prestupnoj godini ima 29 dana.");
+          alert(`Februar u izabranoj tj. prestupnoj godini ima 29 dana.`);
         } else {
-          alert("Februar u normalnoj godini ima 28 dana.");
+          alert(`Februar u izabranoj tj. običnoj godini ima 28 dana.`);
         }
         break;
   
       default:
-        alert("Niste uneli validan naziv meseca.");
+        alert(`Niste uneli validan naziv meseca.`);
     }
   }
   brojDanaUMesecu();
